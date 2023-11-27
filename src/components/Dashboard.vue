f8401d5f6ed7a01c5c6b3edfc0201ffb272623c8
Owen5120 <128174413+Owen5120@users.noreply.github.com>
2023-11-27 11:38:23 +0000

    Add files via upload

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,20 @@
+<!-- eslint-disable -->
 <template>
   <div class="robot-dashboard">
     <h2>Robot Dashboard</h2>
     <div>Status: {{ robotStatus }}</div>
     <div>Location: {{ robotLocation.x }}, {{ robotLocation.y }}</div>
     <div>Path: {{ robotPath }}</div>
+    <div>Battery Level: {{ batteryLevel }}%</div>
+    <div>Sensor Status: {{ sensorStatus }}</div>
+
+    <!-- New Section: Camera View -->
+    <div class="camera-view">
+      <h3>Camera View</h3>
+      <!-- Add your camera view implementation here -->
+      <!-- For example, you can display an image or video stream -->
+      <!-- <img :src="cameraImageUrl" alt="Camera View" /> -->
+    </div>
 
     <!-- Buttons for controlling the robot -->
     <button @click="startRobot" :disabled="robotStatus !== 'Idle'">Start Robot</button>
@@ -21,31 +32,34 @@ export default {
       robotStatus: 'Idle',
       robotLocation: { x: 0, y: 0 },
       robotPath: [],
+      batteryLevel: 75, // Example battery level
+      sensorStatus: 'Normal',
+      cameraImageUrl: 'path_to_camera_image.jpg', // Example camera image URL
     };
   },
   methods: {
     startRobot() {
-      // This should emit a ROS message or call a ROS service to start the robot
+      // Call your ROS service or emit a message to start the robot
       this.publishToROS('/start_robot', {});
     },
     pauseRobot() {
-      // This should emit a ROS message or call a ROS service to pause the robot
+      // Call your ROS service or emit a message to pause the robot
       this.publishToROS('/pause_robot', {});
     },
     stopRobot() {
-      // This should emit a ROS message or call a ROS service to stop the robot
+      // Call your ROS service or emit a message to stop the robot
       this.publishToROS('/stop_robot', {});
     },
     publishToROS(topic, message) {
-      // I would replace this with actual ROS communication code
+      // I would replace the ROS server URL with your actual ROS server URL
       const ros = new ROSLIB.Ros({
-        url: '', // I would replace with the ROS server URL
+        url: 'ws://localhost:9090',
       });
 
       const robotControl = new ROSLIB.Topic({
         ros,
         name: topic,
-        messageType: 'std_msgs/String', // Adjust the message type as needed
+        messageType: 'std_msgs/String', // i would adjust the message type as needed
       });
 
       const robotMessage = new ROSLIB.Message({
@@ -63,5 +77,9 @@ export default {
   padding: 16px;
   border: 1px solid #ccc;
   margin-bottom: 16px;
+}
+
+.camera-view {
+  margin-top: 20px;
 }
 </style>
