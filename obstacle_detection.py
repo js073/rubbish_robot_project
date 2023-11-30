d209fffa129d06b3e9e41d7cf0201a106fa97bc5
616xold <150943444+616xold@users.noreply.github.com>
2023-11-30 07:06:13 -0800

    Update obstacle_detection.py

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -10,10 +10,40 @@ from obstacle_detection_helpers import detect_obstacles, update_map_with_obstacl
 class ObstacleDetectionNode:
     def __init__(self):
         rospy.init_node('obstacle_detection_node', anonymous=True)
-        self.lidar_sub = rospy.Subscriber('/base_scan', LaserScan, self.lidar_callback)
+        self.lidar_sub = rospy.Subscriber('/p3dx/laser/scan', LaserScan, self.lidar_callback)
         self.pose_sub = rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.pose_callback)
         self.grid_pub = rospy.Publisher('/grid_update', OccupancyGrid, queue_size=10)
         self.robot_pose = None
+        self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
+        self.static_obstacles = []
+        
+    def map_callback(self, map_msg):
+        rospy.loginfo("Map callback triggered")
+        grid_height = map_msg.info.height
+        grid_width = map_msg.info.width
+
+        # Create a grid of dimensions [grid_width x grid_height]
+        temp = [[0 for _ in range(grid_height)] for _ in range(grid_width)]
+
+        # Fill the grid with map data
+        for i in range(len(map_msg.data)):
+            grid_x = i % grid_width
+            grid_y = i // grid_width
+            temp[grid_x][grid_y] = map_msg.data[i]
+
+        self.static_obstacles = self.process_map_data(temp)
+
+    def process_map_data(self, map_data):
+        static_obstacles = []
+        for x in range(len(map_data)):
+            for y in range(len(map_data[0])):
+                if map_data[x][y] == 100:  # Occupied cell
+                    static_obstacles.append((x, y))
+        rospy.loginfo(f"Number of static obstacles detected: {len(static_obstacles)}")
+        return list(set(static_obstacles))
+ 
+
+    
 
     def pose_callback(self, data):
         rospy.loginfo("pose callback triggered")
@@ -39,9 +69,11 @@ class ObstacleDetectionNode:
 
         obstacles = detect_obstacles(self.robot_pose, processed_data, grid_resolution = grid_resolution)
         rospy.loginfo("Detected obstacles: %s", str(obstacles))
+        
+        rospy.loginfo("First 20 static obstacles: %s", str(self.static_obstacles[:20]))
 
         # Creating a grid map and update it with detected obstacles
-        grid_map = update_map_with_obstacles(obstacles, grid_size=(4000, 4000), grid_resolution=grid_resolution)
+        grid_map = update_map_with_obstacles(obstacles, self.static_obstacles , grid_size=(4000, 4000))
         
         # Publishing the updated grid map
         self.publish_grid(grid_map)
