9de73cc1a064c6bd52f6eb87dadf0b8152e28613
616xold <150943444+616xold@users.noreply.github.com>
2023-12-02 11:42:33 -0800

    bug fixes for obstacle movement

--- obstacle_mover.py
+++ obstacle_mover.py
@@ -6,33 +6,50 @@ from geometry_msgs.msg import Twist
 class ObstacleMover:
     def __init__(self):
         rospy.init_node('obstacle_mover')
-        self.pub_a = rospy.Publisher('/moving_box_1/cmd_vel', Twist, queue_size=10)
-        self.pub_b = rospy.Publisher('/obstacle_box_2/cmd_vel', Twist, queue_size=10)
+        # Change the topic to match your robot's velocity command topic
+        self.pub = rospy.Publisher('/dynamic_sphere/cmd_vel', Twist, queue_size=10)
         self.rate = rospy.Rate(10)  # 10 Hz
-        self.distance = 30  # meters
-        self.speed = 0.5  # meters per second
+        self.distance = 3  # meters
+        self.speed = 0.6  # meters per second
 
-    def move_obstacle(self, pub, forward):
+    def move_straight(self, forward):
         vel_cmd = Twist()
+        # Positive x is forward, negative x is backward
         vel_cmd.linear.x = self.speed if forward else -self.speed
         distance_moved = 0.0
 
         while distance_moved < self.distance:
-            pub.publish(vel_cmd)
+            self.pub.publish(vel_cmd)
             self.rate.sleep()
             distance_moved += self.speed / 10.0
 
-        # Stopping the obstacle
+        # Stop the robot
         vel_cmd.linear.x = 0
-        pub.publish(vel_cmd)
+        self.pub.publish(vel_cmd)
+
+    def turn(self, clockwise):
+        vel_cmd = Twist()
+        # Positive z is left turn, negative z is right turn
+        vel_cmd.angular.z = -1.57 if clockwise else 1.57  # 90 degrees in radians
+        self.pub.publish(vel_cmd)
+        # Adjust sleep time if necessary for a 90-degree turn
+        rospy.sleep(1)
+
+        # Stop the turn
+        vel_cmd.angular.z = 0
+        self.pub.publish(vel_cmd)
 
     def run(self):
         while not rospy.is_shutdown():
-            # Move forward and back for both obstacles
-            self.move_obstacle(self.pub_a, True)
-            self.move_obstacle(self.pub_b, True)
-            self.move_obstacle(self.pub_a, False)
-            self.move_obstacle(self.pub_b, False)
+            # Move straight then turn left
+            self.move_straight(True)
+            self.turn(False)  # False for counter-clockwise turn (left)
+            self.move_straight(True)
+
+            # Move backward then turn right
+            self.move_straight(False)
+            self.turn(True)  # True for clockwise turn (right)
+            self.move_straight(False)
 
 if __name__ == '__main__':
     mover = ObstacleMover()
